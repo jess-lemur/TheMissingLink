8c87aa2a6a3cb9151a363539057c52945c24a95b
jess-lemur <jess.dahmen@makeschool.com>
2020-03-18 00:15:42 -0700

    Added reading errors practice

---
+++ error_practice.py
@@ -0,0 +1,13 @@
+class Monster:
+
+    def __init_(self, name, catchphrase):
+        self.name = name
+        self.catchphrase = cathphrase
+
+    def speak():
+        print(f"Hello my name is {name} and I say {catchphrase})
+
+monster_nova = Monster(nova)
+print(monster_nova.speak())
+#make lower case
+print(monster_nova.speak().lower())
